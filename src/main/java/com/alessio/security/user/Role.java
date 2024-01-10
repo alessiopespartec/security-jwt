c17f91ed4ed68421e9d1ed5ba40b4a9fd6df1e52
alessiopes <alessio.pes@hotmail.com>
2024-01-10 16:14:11 +0100

    First commit

---
+++ src/main/java/com/alessio/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.alessio.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/alessio/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessio.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
